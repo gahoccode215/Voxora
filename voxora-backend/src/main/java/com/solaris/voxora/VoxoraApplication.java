0da59a5062fff65ab035a4e44ad3420704ad45fc
dominhit215@gmail.com <dominhit215@gmail.com>
2026-05-27 02:23:38 +0700

    first commit

---
+++ voxora-backend/src/main/java/com/solaris/voxora/VoxoraApplication.java
@@ -0,0 +1,13 @@
+package com.solaris.voxora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoxoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoxoraApplication.class, args);
+	}
+
+}
---
+++ voxora-backend/src/test/java/com/solaris/voxora/VoxoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solaris.voxora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoxoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
